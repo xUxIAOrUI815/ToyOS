f970ae11cca13435401cbff0113a7da1ad2f8046
xuxiaorui <1531793838@qq.com>
2025-12-09 19:44:33 +0800

    修改app.c，调用ecall来打印

--- user/app.c
+++ user/app.c
@@ -1,9 +1,39 @@
-// 一个运行在用户态的程序
+#include <stdint.h>
+
+// 封装 syscall 
+int syscall(int which, uint64_t arg0, uint64_t arg1, uint64_t arg2){
+    register uint64_t a0 asm("a0") = arg0;
+    register uint64_t a1 asm("a1") = arg1;
+    register uint64_t a2 asm("a2") = arg2;
+    register uint64_t a7 asm("a7") = which;
+
+    asm volatile("ecall"
+                : "+r"(a0)
+                : "r"(a1), "r"(a2), "r"(a7)
+                : "memory");
+
+    return a0;
+}
+
+// 实现 write 函数
+void sys_write(char *str){
+    int len = 0;
+    while(str[len]) len++;  // 计算长度
+    // 64 是 sys_write 的调用号
+    syscall(64, 1, (uint64_t)str, len);
+}
+
+// 实现 exit 函数
+void sys_exit(int code){
+    // 93 是 sys_exit 的调用号
+    syscall(93, code, 0, 0);
+}
 
 void main(){
-    int a = 10;
-    int b = 20;
-    int c = a + b;
+    // 通过 ecall 请求内核打印
+    sys_write("Hello! I am running in User Mode (U-Mode)! \n");
+    sys_write("I can do System Calls now! \n");
 
-    while(1) {};
+    sys_exit(0);
 }
+
