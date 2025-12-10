4508b568e7b908613bd98e0ed9ceb35eb27855b5
xuxiaorui <1531793838@qq.com>
2025-12-10 10:52:58 +0800

    测试CPU调度是否正常

--- user/app.c
+++ user/app.c
@@ -35,10 +35,24 @@ void sys_yield(){
 }
 
 void main(){
-    // 通过 ecall 请求内核打印
-    sys_write("Hello! I am running in User Mode (U-Mode)! \n");
-    sys_write("I can do System Calls now! \n");
+    // // 通过 ecall 请求内核打印
+    // sys_write("Hello! I am running in User Mode (U-Mode)! \n");
+    // sys_write("I can do System Calls now! \n");
 
+    // sys_exit(0);
+
+    sys_write("Hello, I am a Task! \n");
+
+    // 第一次让出
+    sys_write("I am yielding now... (1/3) \n");
+    sys_yield();    // 让出 CPU，切换到下一个任务
+
+    // 如果 CPU 调度正常，会回到这里继续执行
+    sys_write("I am back! Yielding again... (2/3) \n");
+    sys_yield();
+
+    // 同2
+    sys_write("Back again! Bye~ (3/3) \n");
     sys_exit(0);
 }
 
